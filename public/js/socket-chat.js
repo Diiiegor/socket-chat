d67e0babeb08352734e0b0bd7279c9c796c4f68e
Juan diego <diiegooliveros@gmail.com>
2019-03-31 15:07:43 -0500

    -Add event for listen user message and emit this message to all users

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -32,16 +32,19 @@ socket.on('disconnect', function () {
 
 
 // Enviar información
-socket.emit('enviarMensaje', {
-    usuario: 'Fernando',
-    mensaje: 'Hola Mundo'
-}, function (resp) {
-    console.log('respuesta server: ', resp);
-});
+// socket.emit('crearMensaje', {
+//     usuario: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function (resp) {
+//     console.log('respuesta server: ', resp);
+// });
 
 // Escuchar información
-socket.on('enviarMensaje', function (mensaje) {
-
+socket.on('crearMensaje', function (mensaje) {
     console.log('Servidor:', mensaje);
+});
 
+//cuando un usuario entra o sale del chat recuperatoda la lisa de personas
+socket.on('listaPersonas',function (resp) {
+    console.log(resp)
 });
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,6 +1,8 @@
 const {io} = require('../server');
 const {Usuarios} = require('../clases/Usuarios');
 const usuarios = new Usuarios();
+const {crearMensaje} = require('../utils/utils');
+
 
 io.on('connection', (client) => {
 
@@ -12,8 +14,23 @@ io.on('connection', (client) => {
                 mensaje: 'EL nombre es necesario'
             });
         }
-        const personas=usuarios.agregarPersona(client.id, data.nombre);
+        const personas = usuarios.agregarPersona(client.id, data.nombre);
+        client.broadcast.emit('listaPersonas', usuarios.getPersonas());
         callback(personas);
-    })
+    });
+
+
+    client.on('crearMensaje',(data)=>{
+        const persona=usuarios.getPersona(client.id);
+        const mensaje=crearMensaje(persona.nombre,data.mensaje);
+        client.broadcast.emit('crearMensaje',mensaje);
+    });
+
+
+    client.on('disconnect', () => {
+        const personaBorrada = usuarios.borrarPersona(client.id);
+        client.broadcast.emit('crearMensaje', crearMensaje('Administrador', `${personaBorrada.nombre} salió`));
+        client.broadcast.emit('listaPersonas', usuarios.getPersonas());
+    });
 
 });
